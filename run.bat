850d251816ba320f94e5e606535ecb5533395ced
Aleksandr Izmailov <zinoviy23@gmail.com>
2022-12-09 18:04:14 +0100

    Check on windows

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+
+call gradlew.bat :installDist > nul
+
+if "%~1" == "-e" (GOTO CLEAN_VARS) else (GOTO RUN)
+
+:CLEAN_VARS
+echo "Some cleanup"
+set "IDEA_INITIAL_DIRECTORY="
+set "__INTELLIJ_COMMAND_HISTFILE__="
+set "TERMINAL_EMULATOR="
+call chcp 65001 > nul
+
+:RUN
+set "JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+call .\build\install\terminal-playground\bin\terminal-playground.bat
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,64 @@
 import com.github.ajalt.mordant.animation.animation
+import com.github.ajalt.mordant.table.table
 import com.github.ajalt.mordant.terminal.Terminal
 import com.github.ajalt.mordant.widgets.progressLayout
 import java.lang.management.ManagementFactory
+import java.nio.file.StandardOpenOption
 import java.util.concurrent.TimeUnit
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.writeText
 
 fun main() {
     println("wait for debug")
     Thread.sleep(TimeUnit.SECONDS.toMillis(5))
-    val terminal = Terminal()
-    println("Console = ${System.console()}")
-    println("crClearsLine = ${terminal.info.crClearsLine}")
+    val terminal = Terminal(
+//        interactive = System.console() != null &&
+//                (!System.getenv("WT_SESSION").isNullOrEmpty() ||
+//                "windows" !in System.getProperty("os.name")?.lowercase().orEmpty())
+    )
+    val myTable = table {
+        header {
+            row {
+                cell("Some Information about terminal") {
+                    columnSpan = 2
+                }
+            }
+        }
+        body {
+            row {
+                cell("Console")
+                cell("${System.console()}")
+            }
+            row {
+                cell("crClearsLine")
+                cell("${terminal.info.crClearsLine}")
+            }
+            row {
+                cell("interactive")
+                cell("${terminal.info.interactive}")
+            }
+        }
+    }
+    terminal.println(myTable)
     println("inputArguments = ${ManagementFactory.getRuntimeMXBean().inputArguments}")
     println("----------------env---------------------")
-    println(System.getenv().entries.joinToString("\n") { (key, value) -> "$key = $value"})
+    val env = System.getenv().entries
+        .sortedBy { it.key }
+        .joinToString("\n") { (key, value) -> "$key = $value" }
+    println(env)
+    Path("out")
+        .createDirectories()
+        .resolve("file-${System.currentTimeMillis()}.txt")
+        .writeText(env)
+    println("----------------------------------------")
+    println("------------------props-----------------")
+    println(System.getProperties().entries.sortedBy { it.key.toString() }
+        .joinToString("\n") { (key, value) -> "$key = $value" })
+    println("----------------------------------------")
+    println("Some strange values:")
+    println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    println("┌┬┐├┼┤└┴┘─│╷╵╴╶")
     println("----------------------------------------")
     println("----------------------------------------")
     println("----------------------------------------")
