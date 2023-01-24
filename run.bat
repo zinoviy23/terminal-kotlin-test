0647da21a9e81f4cf747ccb1af5809ffbb2d0c06
Aleksandr Izmailov <zinoviy23@gmail.com>
2023-01-24 15:46:03 +0100

    Windows, Chinese, Emoji

--- run.bat
+++ run.bat
@@ -1,6 +1,16 @@
 @echo off
 
-call gradlew.bat :installDist > nul
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+
+set CURRENT=%cd%
+
+cd %DIRNAME%
+call %DIRNAME%gradlew.bat :installDist > nul
+
+cd %CURRENT%
 
 if "%~1" == "-e" (GOTO CLEAN_VARS) else (GOTO RUN)
 
@@ -15,4 +25,6 @@ call chcp 65001 > nul
 
 :RUN
 set "JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
-call .\build\install\terminal-playground\bin\terminal-playground.bat
+call %DIRNAME%build\install\terminal-playground\bin\terminal-playground.bat %*
+
+if "%OS%"=="Windows_NT" endlocal
